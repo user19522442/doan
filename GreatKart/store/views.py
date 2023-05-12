add5db3b9f3ac42d929f7cad0bfb2239286e8213
user19522442 <19522442@gm.uit.edu.vn>
2023-05-12 09:48:06 +0700

    Phan trang va fix loi khong dinh nghia bien

--- GreatKart/store/views.py
+++ GreatKart/store/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Product,Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def store(request,category_slug=None):
@@ -13,12 +14,19 @@ def store(request,category_slug=None):
         categories = get_object_or_404(Category,slug= category_slug)
         products = Product.objects.filter(category = categories, is_available =True)
         product_count = products.count()
+        paginator = Paginator(products, 2)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        
         product_count = products.count()
         
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request,'store/store.html',context)
