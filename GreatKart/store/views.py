dadb295e3cd74a4f9975c20983953847ec362449
user19522442 <19522442@gm.uit.edu.vn>
2023-05-12 09:11:06 +0700

    Hien thi so san pham da mua

--- GreatKart/store/views.py
+++ GreatKart/store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
 from .models import Product,Category
+from carts.models import CartItem
+from carts.views import _cart_id
 
 # Create your views here.
 def store(request,category_slug=None):
@@ -23,9 +26,12 @@ def store(request,category_slug=None):
 def product_detail(request,category_slug, product_slug): # category__slug to connect its slug
     try:
         single_product = Product.objects.get(category__slug = category_slug,slug = product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product = single_product).exists()
+        # return HttpResponse(in_cart)
     except Exception as e:
         raise e
     context = {
         'single_product':single_product,
+        'in_cart': in_cart,
     }
     return render(request,'store/product_detail.html',context)
