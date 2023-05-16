33d16a305d795c2456132c49d8cb91c5b6e31351
user19522442 <19522442@gm.uit.edu.vn>
2023-05-16 23:41:38 +0700

    Product Variation Model

--- GreatKart/store/models.py
+++ GreatKart/store/models.py
@@ -2,21 +2,41 @@ from django.db import models
 from django.urls import reverse
 from category.models import Category
 
+
 # Create your models here.
 class Product(models.Model):
-    product_name = models.CharField(max_length=200,unique=True)
-    slug = models.SlugField(max_length=200, unique= True)
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, unique=True)
     price = models.IntegerField()
-    images = models.ImageField(upload_to='photos/products')
+    images = models.ImageField(upload_to="photos/products")
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now_add=True)
-    
+
     def get_url(self):
-        return reverse('product_detail', args =[self.category.slug,self.slug])
-    
+        return reverse("product_detail", args=[self.category.slug, self.slug])
+
     def __str__(self):
         return self.product_name
+
+
+variation_category_choice = (
+    ("color", "color"),
+    ("size", "size"),
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(
+        max_length=100, choices=variation_category_choice
+    )
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.product
