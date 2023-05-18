546da0ffce838bae3d337a97e63b63cf0206b186
user19522442 <19522442@gm.uit.edu.vn>
2023-05-18 12:23:43 +0700

    Search function

--- GreatKart/store/views.py
+++ GreatKart/store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Product,Category
+from django.db.models import Q
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -43,3 +44,15 @@ def product_detail(request,category_slug, product_slug): # category__slug to con
         'in_cart': in_cart,
     }
     return render(request,'store/product_detail.html',context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context ={
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request,'store/store.html',context)
