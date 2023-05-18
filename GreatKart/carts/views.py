58b3ee9c66527a85960240babe108f18607c6db9
user19522442 <19522442@gm.uit.edu.vn>
2023-05-18 18:10:08 +0700

    Fix increasement, add cart and grouping Variation

--- GreatKart/carts/views.py
+++ GreatKart/carts/views.py
@@ -36,15 +36,35 @@ def add_cart(request, product_id):
         cart = Cart.objects.create(cart_id=_cart_id(request))
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        if len(product_variation)> 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
-        cart_item.quality += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(product=product,cart=cart)
+        # có tồn tại variation hay là không -> database
+        # variation hiện tại -> product_variation
+        # item_id -> database
+        ex_var_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+    
+        if product_variation in ex_var_list:
+            # tăng số lượng quality của cart item
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id = item_id)
+            item.quality += 1
+            item.save()
+        else:
+            item = CartItem.objects.create(product=product, quality =1,cart=cart)
+            # tạo một cart item mới
+            if len(product_variation)> 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+        # cart_item.quality += 1
+            item.save()
+    else:
         cart_item = CartItem.objects.create(
             product=product,
             quality=1,
@@ -52,30 +72,32 @@ def add_cart(request, product_id):
         )
         if len(product_variation)> 0:
             cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
+            cart_item.variations.add(*product_variation)
         cart_item.save()
     return redirect("cart")
 
 
 # giảm số lượng sản phẩm
-def remove_cart(request, product_id):
+def remove_cart(request, product_id,cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    if cart_item.quality > 1:
-        cart_item.quality -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart, id= cart_item_id)
+        if cart_item.quality > 1:
+            cart_item.quality -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect("cart")
 
 
 # xóa sản phẩm khỏi giỏ hàng
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id,cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
-    product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    product = get_object_or_404(Product, id=product_id,)
+    cart_item = CartItem.objects.get(product=product, cart=cart,id=cart_item_id)
     cart_item.delete()
     return redirect("cart")
 
