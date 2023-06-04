b64e54079362d621c87419ff71015fbb15e70e58
user19522442 <19522442@gm.uit.edu.vn>
2023-06-04 19:02:33 +0700

    fix the dash board and display user information

--- GreatKart/accounts/views.py
+++ GreatKart/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import RegistrationForm
 from .models import Account
+from orders.models import Order
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from carts.models import Cart,CartItem
@@ -170,7 +171,13 @@ def activate(request,uidb64,token):
     
 @login_required(login_url = 'login')
 def dashboard(request):
-  return render(request,  'accounts/dashboard.html')
+  orders = Order.objects.order_by('-created_at').filter(user_id = request.user.id, is_ordered=True)
+  orders_count = orders.count()
+  context = {
+      'orders_count': orders_count,
+  }
+
+  return render(request,  'accounts/dashboard.html',context)
 
 def forgotPassword(request):
     """
